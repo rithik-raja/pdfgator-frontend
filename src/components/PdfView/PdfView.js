7a7c2a9399504fef2ce39e22869b87ae22ff7b03
Rithik Raja <rithik@Rithiks-MacBook-Pro.local>
2023-07-29 20:48:26 -0400

    lots of ui updates

--- src/components/PdfView/PdfView.js
+++ src/components/PdfView/PdfView.js
@@ -1,30 +1,25 @@
-import React, { useState } from "react";
-import { Viewer, Worker } from "@react-pdf-viewer/core";
-// import { defaultLayoutPlugin } from "@react-pdf-viewer/default-layout";
-import { highlightPlugin, Trigger } from "@react-pdf-viewer/highlight";
-import { ListGroup, Container, Col, Row, Form, Button } from "react-bootstrap";
-import * as Icon from "react-feather";
-import CitationModal from "../CitationModal/CitationModal";
+import { useEffect, useState } from "react";
 
-// import type {
-//   HighlightArea,
-//   RenderHighlightsProps,
-// } from "@react-pdf-viewer/highlight";
+import { Viewer, Worker } from "@react-pdf-viewer/core";
+import { highlightPlugin, Trigger } from "@react-pdf-viewer/highlight";
+import { pageNavigationPlugin } from '@react-pdf-viewer/page-navigation';
+import jumpToPagePlugin from "../../utils/jumpToPagePlugin";
+
+import { ListGroup, Container, Col, Row, Form, Button, OverlayTrigger, Tooltip } from "react-bootstrap";
+import * as Icon from "react-feather";
+
+import CitationModal from "../CitationModal/CitationModal";
 
 import "@react-pdf-viewer/core/lib/styles/index.css";
 import "@react-pdf-viewer/highlight/lib/styles/index.css";
 
-// interface RenderHighlightAreasExampleProps {
-//     areas: HighlightArea[];
-//     fileUrl: string;
-// }
+const PdfView = ({ areas, fileUrl, pdfLists }) => {
 
-const Pdf2 = ({ areas, fileUrl, pdfLists }) => {
-  //   const defaultLayoutPluginInstance = defaultLayoutPlugin();
+  let totalPages
 
   const [currentIndex, setcurrentIndex] = useState(-1);
+  const [modalShow, setModalShow] = useState(false);
 
-  const [modalShow, setModalShow] = React.useState(false);
   const renderHighlights = (props) => (
     <div
       className="rpv-core__viewer"
@@ -68,6 +63,13 @@ const Pdf2 = ({ areas, fileUrl, pdfLists }) => {
     trigger: Trigger.None,
   });
   const { jumpToHighlightArea } = highlightPluginInstance;
+
+  const pageNavigationPluginInstance = pageNavigationPlugin();
+  const { CurrentPageLabel } = pageNavigationPluginInstance;
+
+  const jumpToPagePluginInstance = jumpToPagePlugin();
+  const { jumpToPage } = jumpToPagePluginInstance;
+
   const moveResult = (isNext) => {
     if (areas.indices.length > 0) {
       let currentVal = isNext ? currentIndex + 1 : currentIndex - 1;
@@ -77,12 +79,39 @@ const Pdf2 = ({ areas, fileUrl, pdfLists }) => {
       }
     }
   };
+
   const jumpResult = (ind) => {
     if (ind >= 0 && ind < areas.indices.length) {
       jumpToHighlightArea(areas.bboxes[areas.indices[ind]]);
       setcurrentIndex(ind);
     }
   };
+
+  const handleJumpPage = (event) => {
+    if (event.key === "Enter") {
+      let page = document.getElementById("page-number-input").value
+      page = parseInt(page)
+      if (Number.isInteger(page) && page <= totalPages && page > 0) {
+        console.log(page)
+        jumpToPage(page - 1)
+      }
+    }
+  }
+
+  const SearchBarButton = ({ text, IconComponent, onClickFunc }) => {
+    const renderPopover = (props) => (
+      <Tooltip id="button-tooltip" {...props}>
+        {text}
+      </Tooltip>
+    );
+    return (
+      <OverlayTrigger overlay={renderPopover}>
+        <Button variant="light" onClick={onClickFunc}>
+          <IconComponent color="black" />
+        </Button>
+      </OverlayTrigger>
+    )
+  }
 
   return (
     <>
@@ -94,7 +123,7 @@ const Pdf2 = ({ areas, fileUrl, pdfLists }) => {
                 <>
                   <Viewer
                     fileUrl={fileUrl}
-                    plugins={[highlightPluginInstance]}
+                    plugins={[highlightPluginInstance, pageNavigationPluginInstance, jumpToPagePluginInstance]}
                   />
                 </>
               ) : (
@@ -105,67 +134,37 @@ const Pdf2 = ({ areas, fileUrl, pdfLists }) => {
             </Col>
 
             <Col className="d-none d-lg-block col-lg-3 py-2 right-sidebar">
-              {/* <div>
-                <ul className="right-sidebar-list">
-                  <li
-                    className="right-sidebar-list-item active"
-                    onClick={() => moveResult(true)}
-                  >
-                    <Icon.ArrowUpCircle />
-                    <span>Next result</span>
-                  </li>
-                  <li
-                    className="right-sidebar-list-item"
-                    onClick={() => moveResult(false)}
-                  >
-                    <Icon.ArrowDownCircle />
-                    <span> Prev result</span>
-                  </li>
-                  <li className="right-sidebar-list-item">
-                    <Icon.ZoomIn />
-                    <span> Narrow search</span>
-                  </li>
-                  <li
-                    className="right-sidebar-list-item"
-                    onClick={() => setModalShow(true)}
-                  >
-                    <Icon.Book />
-                    <span>Generate citation</span>
-                  </li>
-                  <CitationModal
-                    pdflists={pdfLists}
-                    show={modalShow}
-                    onHide={() => setModalShow(false)}
-                  />
-                </ul>
-              </div> */}
               <ListGroup as="ol" numbered>
                 {
                   areas.previews.map(
                     (preview, ind) =>
-                      <ListGroup.Item onClick={() => jumpResult(ind)} as="li" key={ind} className="right-sidebar-button mx-2 my-1">{preview}</ListGroup.Item>
+                      <ListGroup.Item onClick={() => jumpResult(ind)} style={{boxShadow: (ind === currentIndex) ? "0 4px 8px rgba(0, 0, 0, 0.4)" : "none"}} as="li" key={ind} className="right-sidebar-button mx-2 my-1">{preview}</ListGroup.Item>
                   )
                 }
               </ListGroup>
             </Col>
           </Row>
         </Container>
+
         <Container fluid className="searchbar-container position-relative">
           <Row>
             <Col className="col-lg-9 px-4">
               <Container fluid className="searchbar-container-inner">
-                <div className="d-flex" style={{width: "100%"}}>
-                  <div style={{marginRight: "auto"}}>
-                    <Button>
-                      <Icon.Book />
-                    </Button>
+                <div className="d-flex mb-2 align-items-center " style={{width: "100%"}}>
+                  <div style={{marginRight: "0.5rem"}}>
+                    <SearchBarButton text="Delete File" IconComponent={Icon.Trash} onClickFunc={() => {}}/>
                   </div>
-                  <Button>
-                    <Icon.ArrowLeft />
-                  </Button>
-                  <Button>
-                    <Icon.ArrowRight />
-                  </Button>
+                  <div style={{marginRight: "auto", display: "flex", alignItems: "center"}}>
+                    <input onKeyDown={handleJumpPage} defaultValue={1} style={{marginRight: "0.2rem"}} id="page-number-input" className="plain-input" />
+                    <span id="total-pages-span">of</span>
+                  </div>
+                  <div style={{marginRight: "0.5rem"}}>
+                    <SearchBarButton text="Citations & References" IconComponent={Icon.Book} onClickFunc={() => {}}/>
+                  </div>
+                  <div style={{marginRight: "0.5rem"}}>
+                    <SearchBarButton text="Previous Result" IconComponent={Icon.ArrowLeft} onClickFunc={() => moveResult(false)}/>
+                  </div>
+                  <SearchBarButton text="Next Result" IconComponent={Icon.ArrowRight} onClickFunc={() => moveResult(true)}/>
                 </div>
                 <Form className="d-flex w-100">
                   <Form.Control
@@ -173,6 +172,7 @@ const Pdf2 = ({ areas, fileUrl, pdfLists }) => {
                     placeholder="Ask any question..."
                     className="me-2"
                     aria-label="Search"
+                    style={{border: 0, boxShadow: "none"}}
                   />
                   <Button>
                     Search
@@ -183,9 +183,20 @@ const Pdf2 = ({ areas, fileUrl, pdfLists }) => {
             <Col className="d-none d-lg-block col-lg-3" />
           </Row>
         </Container>
+
+        <CurrentPageLabel>
+          {
+            (props) => {
+              totalPages = props.numberOfPages
+              document.getElementById("page-number-input").value = props.currentPage + 1
+              document.getElementById("total-pages-span").innerText = `of ${totalPages}`
+            }
+          }
+        </CurrentPageLabel>
+
       </Worker>
     </>
   );
 };
 
-export default Pdf2;
+export default PdfView;
