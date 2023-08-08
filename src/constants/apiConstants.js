c08463ab07ba7c2e9b72bda8f63444a9414c475c
Rithik Raja <rithik@Rithiks-MacBook-Pro.local>
2023-08-08 10:12:43 -0400

    handle delete file

--- src/constants/apiConstants.js
+++ src/constants/apiConstants.js
@@ -3,3 +3,5 @@ export const GET_FILES = "api/data/filelist/";
 export const SET_FILES = "api/data/files/";
 export const GOOGLE_OAUTH = "api/register-by-access-token/social/google-oauth2/";
 export const SEARCH_QUERY = "api/data/searchquery/"
+export const GET_USER = "api/getuser/"
+export const DELETE_FILES = "api/data/deletefiles/";
