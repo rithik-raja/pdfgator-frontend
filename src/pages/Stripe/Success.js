75a7183b6ae8c1c0050f05bc42198511d4b8c19d
Kiruthika Senniappan <kiruthika.senniappan@impigertech.com>
2023-09-22 19:18:37 +0530

    url change

--- src/pages/Stripe/Success.js
+++ src/pages/Stripe/Success.js
@@ -13,7 +13,9 @@ const Success = () => {
   console.log(currentSessionId);
   const [isValid, setisValid] = useState(null);
   const verifySession = async () => {
-    const res = await get(VERIFY_CHECKOUT + currentSessionId + "/");
+    const res = await get(
+      VERIFY_CHECKOUT + "?session_id=" + currentSessionId + "/"
+    );
     console.log(res);
     if (res?.data && res?.data?.data) {
       if (Response?.data?.data?.is_valid === true) {
@@ -30,22 +32,22 @@ const Success = () => {
   }, [currentSessionId]);
   const [seconds, setseconds] = useState(5);
 
-  setInterval(function () {
-    if (isValid !== null) {
-      let newSec = seconds - 1;
-      setseconds(newSec);
-      if (newSec === 0) {
-        navigate("/", { replace: true });
-        return 1;
-      }
-    }
-  }, 1000);
+  // setInterval(function () {
+  //   if (isValid !== null) {
+  //     let newSec = seconds - 1;
+  //     setseconds(newSec);
+  //     if (newSec === 0) {
+  //       navigate("/", { replace: true });
+  //       return 1;
+  //     }
+  //   }
+  // }, 1000);
   function TopContainer({ isValid }) {
     if (isValid === null) {
       return (
         <div
           className="top-container"
-          style={{ padding: "80px 20px", color: "white" }}
+          style={{ padding: "100px 20px", color: "white" }}
         >
           <CustomSpinner />
           <h4 className="mt-5">Loading...</h4>
@@ -54,8 +56,8 @@ const Success = () => {
     } else if (isValid === true) {
       return (
         <div
-          className="top-container-success"
-          style={{ padding: "80px 20px", color: "white" }}
+          className="top-container success-bg"
+          style={{ padding: "100px 20px", color: "white" }}
         >
           <Icon.CheckCircle size={"100px"} color="white" />
           <h4 className="mt-5">PAYMENT SUCCESS</h4>
@@ -64,11 +66,11 @@ const Success = () => {
     } else {
       return (
         <div
-          className="top-container-failed"
-          style={{ padding: "80px 20px", color: "white" }}
+          className="top-container failed-bg"
+          style={{ padding: "100px 20px", color: "white" }}
         >
           <Icon.XCircle size={"100px"} color="white" />
-          <h4 className="mt-5">PAYMENT FAILED</h4>
+          <h4 className="mt-5">Something Went Wrong</h4>
         </div>
       );
     }
@@ -80,25 +82,24 @@ const Success = () => {
         {`
       body {
         min-height: 100vh;
-        padding-top: 2rem;
       }     
-      .top-container-success{
+      .success-bg{
         background-image: radial-gradient(circle at -1% 57.5%, #13aa52 0%, #00662b 90%);
       }
-      .top-container-failed{
+      .failed-bg{
         background-image: radial-gradient(circle at -1% 57.5%, #fb2056 0%, #870223 90%);
       }
 
     `}
       </style>
-      <div className="container">
+      <div className="container-fluid p-0">
         <div className="h-100  d-flex align-items-center justify-content-center">
           <div className="w-100">
             <TopContainer isValid={isValid} />
 
-            <div className="mt-4 bottom-container">
+            <div className="mt-5 bottom-container">
               <p style={{ color: "#3f51b5" }}>
-                You will be redirected to the home page in {seconds} seconds
+                {/* You will be redirected to the home page */}
               </p>
               <Button
                 className="mt-1 btn-secondary "
